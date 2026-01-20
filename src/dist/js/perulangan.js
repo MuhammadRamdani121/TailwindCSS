a080160be3cbcec6ae6351694b0bc1556a79b069
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-01-20 18:41:11 +0700

    Menambahkan For part2

--- src/dist/js/perulangan.js
+++ src/dist/js/perulangan.js
@@ -1,35 +1,55 @@
-let angkot = 2;
+// let angkot = 2;
 
-for (let angkot = 0; angkot <= 4; angkot++) {
-  console.log(angkot + "<br>");
-}
+// for (let angkot = 0; angkot <= 4; angkot++) {
+//   console.log(angkot + "<br>");
+// }
 
-// perulangan genap
-for (let i = 0; i <= 20; i += 2) {
-  console.log(i);
-}
-// perulangan ganjil
-for (let i = 1; i <= 20; i += 2) {
-  console.log(i);
-}
-// perulangan mines
-for (let i = 20; i <= 0; i -= 2) {
-  console.log(i);
-}
-// perulangan perkalian
-for (let i = 10; i <= 100000; i *= 10) {
-  console.log(i);
-}
+// // perulangan genap
+// for (let i = 0; i <= 20; i += 2) {
+//   console.log(i);
+// }
+// // perulangan ganjil
+// for (let i = 1; i <= 20; i += 2) {
+//   console.log(i);
+// }
+// // perulangan mines
+// for (let i = 20; i <= 0; i -= 2) {
+//   console.log(i);
+// }
+// // perulangan perkalian
+// for (let i = 10; i <= 100000; i *= 10) {
+//   console.log(i);
+// }
 // perulangan array
-const animal = ["kucing", "anjing", "burung"];
+// const animal = ["kucing", "anjing", "burung"];
 
-for (let i = 0; i < animal.length; i++) {
-  console.log(i + 1, animal[i]);
-}
-let str = "LOL";
-for (let i = 0; i <= 4; i++) {
-  console.log("Outer: ", i);
-  for (let j = 0; j < str.length; j++) {
-    console.log("Inner: ", str[j]);
+// for (let i = 0; i < animal.length; i++) {
+//   console.log(i + 1, animal[i]);
+// }
+// let str = "LOL";
+// for (let i = 0; i <= 4; i++) {
+//   console.log("Outer: ", i);
+//   for (let j = 0; j < str.length; j++) {
+//     console.log("Inner: ", str[j]);
+//   }
+// }
+
+const pilihan = "abcd";
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i} . Soal Nomor ${i}:`);
+  for (let j = 0; j < pilihan.length; j++) {
+    console.log(`${pilihan[j]}. Pilihan Jawaban`);
   }
+}
+
+// Mendapatkan Data Dari Nested Array Dengan Nested Loop
+const studentRow = [
+  ["Hani", "Siti", "Budi"],
+  ["Andi", "Rina", "Dewi"],
+  ["Eka", "Fina", "Gina"],
+];
+
+for (let i = 0; i < studentRow.length; i++) {
+  const row = studentRow[i];
+  console.log(row);
 }
