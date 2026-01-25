527566da2c5c0c65f8337901892e7f07fa692f6b
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-01-25 15:28:59 +0700

    menambahkan arrowFunction.js

---
+++ src/dist/js/arrowFunction.js
@@ -0,0 +1,16 @@
+// function declaration
+function perpangkatan(x) {
+  return x * x;
+}
+// function call
+const hasil = perpangkatan(5);
+
+// function Expression
+const hasil = function (x) {
+  return x * x;
+};
+
+// arrow function
+const perpangkatan = (x) => {
+  return x * x;
+};
